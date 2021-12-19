8ca4709d2c44f95e710d6d50a97055c9a573f5e9
Yehiyam Livneh <yehiyam@gmail.com>
2021-12-19 15:16:02 +0200

    update

--- Formula/hkubectl.rb
+++ Formula/hkubectl.rb
@@ -9,7 +9,7 @@ class Hkubectl < Formula
       sha256 "46c9cd60fd79e3af3eeb849d3557fc4e2f4ce6c8b6a741441392242e12761e36"
 
       def install
-        bin.install "hkubectl"
+        bin.install "hkubectl-macos" => "hkubectl"
 
         # Install bash completion
         output = Utils.popen_read("#{bin}/hkubectl completion bash")
@@ -28,7 +28,7 @@ class Hkubectl < Formula
       sha256 "4cf8630c8e50fbcbda88d9cee1763708d9aff8fde2f5fb0563a9ffe464b4607d"
 
       def install
-        bin.install "hkubectl"
+        bin.install "hkubectl-linux" => "hkubectl"
 
         # Install bash completion
         output = Utils.popen_read("#{bin}/hkubectl completion bash")
