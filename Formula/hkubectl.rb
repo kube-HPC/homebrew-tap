baeacc0ac3afc40d5becc08c1851a3c74ca6adcc
yehiyam <yehiyam@users.noreply.github.com>
2022-02-14 14:48:33 +0200

    version

--- Formula/hkubectl.rb
+++ Formula/hkubectl.rb
@@ -5,8 +5,8 @@ class Hkubectl < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kube-HPC/hkubectl/releases/download/v1.1.72/hkubectl-macos"
-      sha256 "1e4fa5512d29e0e64e927f2868d32e529fb4cd0a1724c407f700e00d80118ccd"
+      url "https://github.com/kube-HPC/hkubectl/releases/download/v1.1.74/hkubectl-macos"
+      sha256 "c57c05f07741de44fbf45ac6e06b0ca192af2f076e5aca91f5f1cad35262f58e"
 
       def install
         bin.install "hkubectl-macos" => "hkubectl"
@@ -24,8 +24,8 @@ class Hkubectl < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kube-HPC/hkubectl/releases/download/v1.1.72/hkubectl-linux"
-      sha256 "2b32ee261b27731f9e0057ee442f555e45fe77279cf0cb602a1639970a7bc217"
+      url "https://github.com/kube-HPC/hkubectl/releases/download/v1.1.74/hkubectl-linux"
+      sha256 "b3e2b6fc1937df3625314175b5021ac89653159ca6893a26cd7f67bd16c8ffac"
 
       def install
         bin.install "hkubectl-linux" => "hkubectl"
